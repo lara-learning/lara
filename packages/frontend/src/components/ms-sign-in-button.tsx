2147c42075799a2374bd33ffa3b5a91267986b83
alex.opoku <alex.opoku@sinnerschrader.com>
2023-06-07 11:59:33 +0200

    feat: set client id for env and fix format job

--- packages/frontend/src/components/ms-sign-in-button.tsx
+++ packages/frontend/src/components/ms-sign-in-button.tsx
@@ -1,37 +1,30 @@
-import React, {ButtonHTMLAttributes} from 'react'
+import React, { ButtonHTMLAttributes } from 'react'
 import AppHistory from '../app-history'
-import {PrimaryButton} from './button'
-import {useMsal} from '@azure/msal-react'
-import {loginRequest} from '../hooks/ms-auth'
-import {useAuthentication} from '../hooks/use-authentication'
-import {useLoginPageLoginMutation} from '../graphql'
-import {useIsAuthenticated} from '@azure/msal-react'
+import { PrimaryButton } from './button'
+import { useMsal } from '@azure/msal-react'
+import { loginRequest } from '../hooks/ms-auth'
+import { useAuthentication } from '../hooks/use-authentication'
+import { useLoginPageLoginMutation } from '../graphql'
+import { useIsAuthenticated } from '@azure/msal-react'
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   fullsize?: boolean
 }
 
 export const SignInButton: React.FunctionComponent<ButtonProps> = () => {
-  const {instance, accounts} = useMsal()
-  const {login} = useAuthentication()
+  const { instance } = useMsal()
+  const { login } = useAuthentication()
   const [mutate] = useLoginPageLoginMutation()
   const isAuthenticated = useIsAuthenticated()
 
   const handleLogin = async () => {
-    instance.loginPopup(loginRequest).catch((e) => {
-      console.log(e)
-    })
+    const loginResponse = await instance.loginPopup(loginRequest)
     if (isAuthenticated) {
-      const request = {
-        ...loginRequest,
-        account: accounts[0],
-      }
-      const authResult = await instance.ssoSilent(request)
-      const email = authResult.account?.username
+      const email = loginResponse.account?.username
       if (email) {
-        mutate({variables: {email}})
+        mutate({ variables: { email } })
           .then((response) => {
-            const {data} = response
+            const { data } = response
             if (!data?.login) {
               return AppHistory.getInstance().push('/no-user-found')
             }
@@ -43,6 +36,5 @@ export const SignInButton: React.FunctionComponent<ButtonProps> = () => {
       }
     }
   }
-  return <PrimaryButton onClick={() => handleLogin()}>sign in with
-    ACN</PrimaryButton>
+  return <PrimaryButton onClick={() => handleLogin()}>sign in with ACN</PrimaryButton>
 }
