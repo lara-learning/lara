a247cd8050c9f16f5ab2e7adb296b0393be26787
alex.opoku <alex.opoku@sinnerschrader.com>
2023-06-28 13:06:57 +0200

    fix: fullscreen login

--- packages/frontend/src/frontend.tsx
+++ packages/frontend/src/frontend.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { App } from './app'
-
-ReactDOM.render(<App />, document.getElementById('app'))
+if (window.location.hash !== ''){
+  console.log("hash found" + window.location.hash);
+}
+else
+{
+  ReactDOM.render(<App/>, document.getElementById('app'))
+}
 
 if (module.hot && ENVIRONMENT.debug) {
   module.hot.accept()
