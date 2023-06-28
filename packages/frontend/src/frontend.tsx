e8fba077ec07cb816ededca7171c842ab4786510
alex.opoku <alex.opoku@sinnerschrader.com>
2023-06-28 13:34:35 +0200

    chore: formatting

--- packages/frontend/src/frontend.tsx
+++ packages/frontend/src/frontend.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { App } from './app'
-if (window.location.hash !== ''){
-  console.log("hash found" + window.location.hash);
-}
-else
-{
-  ReactDOM.render(<App/>, document.getElementById('app'))
+
+if (window.location.hash === '') {
+  ReactDOM.render(<App />, document.getElementById('app'))
 }
 
 if (module.hot && ENVIRONMENT.debug) {
