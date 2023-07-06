908f9ae0bf31d10daae8583cefbc96aaba409e6f
alex.opoku <alex.opoku@sinnerschrader.com>
2023-07-06 14:27:16 +0200

    fix: format

--- packages/frontend/src/hooks/ms-auth.ts
+++ packages/frontend/src/hooks/ms-auth.ts
@@ -2,7 +2,9 @@ export const msalConfig = {
   auth: {
     clientId: ENVIRONMENT.microsoftClientID ?? '',
     authority: 'https://login.microsoftonline.com/' + ENVIRONMENT.microsoftTenantID,
-    knownAuthorities: ['https://login.microsoftonline.com/' + ENVIRONMENT.microsoftTenantID + '/v2.0/.well-known/openid-configuration'],
+    knownAuthorities: [
+      'https://login.microsoftonline.com/' + ENVIRONMENT.microsoftTenantID + '/v2.0/.well-known/openid-configuration',
+    ],
     redirectUri: ENVIRONMENT.frontendUrl,
   },
   cache: {
