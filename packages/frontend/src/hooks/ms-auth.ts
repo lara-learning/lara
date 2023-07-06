fb3a8abf7d29ec2e87e21687597f9753321b6314
alex.opoku <alex.opoku@sinnerschrader.com>
2023-07-06 12:28:46 +0200

    fix: add knownAuthorities

--- packages/frontend/src/hooks/ms-auth.ts
+++ packages/frontend/src/hooks/ms-auth.ts
@@ -2,6 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: ENVIRONMENT.microsoftClientID ?? '',
     authority: 'https://login.microsoftonline.com/' + ENVIRONMENT.microsoftTenantID,
+    knownAuthorities: ['https://login.microsoftonline.com'],
     redirectUri: ENVIRONMENT.frontendUrl,
   },
   cache: {
