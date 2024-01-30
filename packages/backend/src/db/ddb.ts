bec978e7858c3eca3a00ae1f96a681efb6973d33
Tim Brust <tim.brust@accenture.com>
2024-01-30 11:37:33 +0100

    chore(deps-dev): update outdated serverless dynamodb plugin (#61)
    * chore(deps-dev): update outdated serverless dynamodb plugin
    
    * chore(deps-dev): update outdated serverless offline plugin
    
    * fix: update dummy access key for DynamoDB
    
    * chore: added timeout to serverless.yml
    
    ---------
    
    Co-authored-by: CassianKnoth <cassian.knoth@accenture.com>

--- packages/backend/src/db/ddb.ts
+++ packages/backend/src/db/ddb.ts
@@ -6,8 +6,8 @@ const params = IS_OFFLINE
   ? {
       region: 'localhost',
       endpoint: 'http://localhost:8000',
-      accessKeyId: 'DEFAULT_ACCESS_KEY',
-      secretAccessKey: 'DEFAULT_SECRET',
+      accessKeyId: 'DEFAULTACCESSKEY',
+      secretAccessKey: 'DEFAULTSECRET',
     }
   : {}
 
