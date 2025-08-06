75392b03cb1e71461adc524694a0b2ab841e9c62
Alexander Schefe <148041958+alexander-schefe@users.noreply.github.com>
2025-08-06 17:13:54 +0200

    chore(all): update i18next dependency (backend) (#155)
    * chore: update i18next dependency (backend)
    
    * fix(i18n): remove non-existing TFunctionResult import and fix generic typing of translation function `t`
    
    * fix: revert email.service.ts
    
    * chore: update i18next dependency (backend)
    
    * fix(i18n): remove non-existing TFunctionResult import and fix generic typing of translation function `t`
    
    * fix: revert email.service.ts
    
    * fix: fixed formatting error
    
    * fix: remove package-lock
    
    ---------
    
    Co-authored-by: Ada <a_disney_da@hotmail.com>

--- packages/backend/src/i18n/index.ts
+++ packages/backend/src/i18n/index.ts
@@ -1,4 +1,4 @@
-import i18next, { TFunctionResult, TOptions } from 'i18next'
+import i18next, { TOptions } from 'i18next'
 import { EmailTranslations, PrintTranslations } from '@lara/api'
 
 import { GermanTranslations } from './de'
@@ -46,17 +46,13 @@ i18next.init({
   },
 })
 
-export type TFunction = <TResult extends TFunctionResult = string>(
-  key: string,
-  lng?: string,
-  params?: TOptions
-) => TResult
+export type TFunction = <TResult = string | object>(key: string, lng?: string, params?: TOptions) => TResult
 
-export const t: TFunction = (key, lng, params) => {
-  return i18next.t(key, { lng: lng ?? 'de', ...params })
+export const t = <TResult = string | object>(key: string, lng?: string, params?: TOptions): TResult => {
+  return i18next.t(key, { lng: lng ?? 'de', ...params }) as unknown as TResult
 }
 
-type SimpleTFuntion = <TResult extends TFunctionResult = string>(key: string, params?: TOptions) => TResult
+type SimpleTFuntion = <TResult = string | object>(key: string, params?: TOptions) => TResult
 
 export const createT = (lng?: string): SimpleTFuntion => {
   return (key, params) => t(key, lng, params)
