f5047c2d7817dbcba5d7bb87c769e1f7a17a2212
Ada <a_disney_da@hotmail.com>
2025-10-02 00:08:33 +0200

    feat: enable user notifications by default

--- packages/frontend/src/components/application-settings.tsx
+++ packages/frontend/src/components/application-settings.tsx
@@ -37,7 +37,7 @@ const ApplicationSettings: React.FunctionComponent<ApplicationSettingsProps> = (
   const updateNotifications = () =>
     updateApplicationSettings({
       variables: {
-        notification: user.notification,
+        notification: !user.notification,
       },
     })
 
