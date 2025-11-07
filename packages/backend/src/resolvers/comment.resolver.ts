0fb639b1b559ae278aa914ec62951279baab3f5d
thegreenmilecomposer <elise.engel@accenture.com>
2025-11-07 13:53:35 +0100

    fix(backend): published null set to true

--- packages/backend/src/resolvers/comment.resolver.ts
+++ packages/backend/src/resolvers/comment.resolver.ts
@@ -70,6 +70,7 @@ export const commentResolver: GqlResolvers<AuthenticatedContext> = {
         ? {
             ...report,
             comments: report?.comments.map((com) => {
+              if (com.published == null) com.published = true
               if (com.published === false) {
                 return com
               } else {
